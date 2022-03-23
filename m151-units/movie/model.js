288832cf68f030efbce83b5b437875c1182085cd
floriangoller <Florian_Goller@sluz.ch>
2022-03-23 10:57:07 +0100

    All Functions for movie

--- m151-units/movie/model.js
+++ m151-units/movie/model.js
@@ -1,44 +1,92 @@
-import mysql from "mysql2/promise"; 
+import mysql from "mysql2/promise";
 
-const connection = await mysql.createConnection({ 
-    host: "127.0.0.1",   
-    user: "root",   
-    password: "sml12345", 
-    database: "movie-db"  , 
-}); 
 
-await connection.connect(); 
 
-export async function getAll() {      
-    const query = 'SELECT * FROM Movies';   
-    const [data]  = await connection.query(query);   
-    return data; 
-} 
+const connection = await mysql.createConnection({
 
-async function insert(movie) {  
-    const query = 'INSERT INTO Movies (title, year) VALUES (?, ?)';
-    const [result]  = await connection.query(query, [movie.title, movie.year]);   
-    return {...movie, id: result.insertId }; 
-} 
+  host: "127.0.0.1",
 
-async function update(movie) {  TODO   } 
+  user: "root",
 
-export async function get(id) {  
-    const query = 'SELECT * FROM Movies WHERE ID = ' + id;   
-    const [data]  = await connection.query(query);   
-    return data; 
-} 
+  password: "sml12345",
+
+  database: "movie-db",
+
+});
+
+
+
+await connection.connect();
+
+
+
+export async function getAll() {
+
+  const query = "SELECT * FROM Movies";
+
+  const [data] = await connection.query(query);
+
+  return data;
+
+}
+
+
+
+async function insert(movie) {
+
+  const query = "INSERT INTO Movies (title, year) VALUES (?, ?)";
+
+  const [result] = await connection.query(query, [movie.title, movie.year]);
+
+  return { ...movie, id: result.insertId };
+
+}
+
+
+
+async function update(movie) {
+
+  const query = "UPDATE Movies SET Title = ?, Year = ?";
+
+  const [data] = [movie.title, movie.year];
+
+  connection.query(query, data);
+
+}
+
+
+
+export async function get(id) {
+
+  const query = "SELECT * FROM Movies WHERE ID = ?";
+
+  const [data] = [id];
+
+  connection.query(query, data);
+
+}
+
+
 
 export async function remove(id) {
-    /*
-    const query = 'DELETE FROM Movies WHERE ID = ' + id;   
-    const [data]  = await connection.query(query);   
-    return data; */
-} 
+
+  const query = "DELETE FROM Movies WHERE ID = ?";
+
+  const [data] = [id];
+
+  connection.query(query, data);
+
+}
+
+
 
 export function save(movie) {
-    /*
-    const query1 = 'UPDATE Movies SET Title = ' + movie.title + ', Year = ' + movie.year + 'WHERE ID = ' + id;
-    const [data]  = connection.query(query);   
-    return data; */
+
+  /*
+
+  What should this function do??
+
+  */
+
 }
+
